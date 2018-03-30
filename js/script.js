78742c1cc12c647dc24887cacda72e9644cc81de
Muna Mohamed <munamohamed94@hotmail.co.uk>
2018-03-30 20:14:42 +0100

    Input the inner values of nyt-header and nyt-articles and create variable to store nyt api url

--- js/script.js
+++ js/script.js
@@ -3,5 +3,11 @@ function loadData() {
     var $nytArticles = $('#nyt-articles');
     var searchVal = $('#search-box').val();
 
+    $nytArticles.text("");
+
+    var nyTimesURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchVal + "&sort=newest&api-key=c44cedace0b44287982782e369b3e3bf";
+
+    $nytHeader.text("New York Times articles about " + searchVal);
+
 
 };
